9b5fc97c0a3c5baeeb534e2638db3d9e615d629f
Mikaylahasal <mikayla.k.hasal@gmail.com>
2018-11-08 15:42:31 -0800

    fixed route back

--- server/index.js
+++ server/index.js
@@ -11,14 +11,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../public'))
 
-app.get("/Instructors/:courseId", (req, res) => {
-  Instructors.find({courseId: req.params.courseId}, (err, result) => {
-    if (err) {
+app.get("/Instructors", (req, res) => {
+  Instructors.count().exec(function (err, count) {
+  var random = Math.floor(Math.random() * count)
+
+  Instructors.findOne()
+  .skip(random)
+  .exec(function (err, result) {
+       if (err) {
       console.log('Error')
     }
     res.status(200).send(JSON.stringify(result))
+    })
   })
-});
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
